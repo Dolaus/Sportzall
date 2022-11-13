d9bd947cfa4d8ea34d0769aba901b52bdcb1de0c
Petro <dsadasdasdas19@gmail.com>
2022-11-14 01:03:50 +0200

    UpdateTrenerPanelDeleteUser&&MyTeam

--- Sportzall/Controllers/TrenerController.cs
+++ Sportzall/Controllers/TrenerController.cs
@@ -25,21 +25,50 @@ namespace Sportzall.Controllers
             {
                 return NotFound();
             }
-            var User = _dbContext.User.Find(id);
+            var UserTeam = _dbContext.User.Find(id);
             if (User == null)
             {
                 return NotFound();
             }
-            TrenersUser trenersUser = new TrenersUser() { 
-            UnicKey=User.Id,
-            Name=User.Name,
-            Year=User.Year,
-            UserId=User.Id
-            };
+            User Trener = _dbContext.User.FirstOrDefault(u=>u.Email==User.Identity.Name);
+            if (!_dbContext.TrenersUser.Any(u => u.UnicKey == UserTeam.Id))
+            {
+                TrenersUser trenersUser = new TrenersUser()
+                {
+                    UnicKey = UserTeam.Id,
+                    Name = UserTeam.Name,
+                    Year = UserTeam.Year,
+                    UserId = Trener.Id
+                };
 
-            _dbContext.TrenersUser.Add(trenersUser);
-            _dbContext.SaveChanges();
+
+                _dbContext.TrenersUser.Add(trenersUser);
+                _dbContext.SaveChanges();
+            }
             return RedirectToAction(nameof(AllUser));
+        }
+        [HttpGet]
+        public IActionResult MyTeam()
+        {
+            User Trener = _dbContext.User.FirstOrDefault(u => u.Email == User.Identity.Name);
+            var team = _dbContext.TrenersUser.Where(u => u.UserId == Trener.Id);
+            return View(team);
+        }
+        [HttpGet]
+        public IActionResult DeleteFromTeam(int? id)
+        {
+            if (id==null||id==0)
+            {
+                return NotFound();
+            }
+            var user = _dbContext.TrenersUser.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _dbContext.TrenersUser.Remove(user);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(MyTeam));
         }
     }
 }
