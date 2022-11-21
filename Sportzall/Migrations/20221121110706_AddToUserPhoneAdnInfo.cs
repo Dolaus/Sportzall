ede0609a72a956be7eece989a533a148742c315b
DolausNot <dsadasdasdas19@gmail.com>
2022-11-21 13:44:07 +0200

    UpdateModelUser

---
+++ Sportzall/Migrations/20221121110706_AddToUserPhoneAdnInfo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportzall.Migrations
+{
+    public partial class AddToUserPhoneAdnInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShortInfo",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ShortInfo",
+                table: "User");
+        }
+    }
+}
--- Sportzall/Models/User.cs
+++ Sportzall/Models/User.cs
@@ -13,6 +13,8 @@ namespace Sportzall.Models
         public int? Year { get; set; }
         [Required]
         public string Password { get; set; }
+        public string? ShortInfo { get; set; }
+        public string? PhoneNumber { get; set; }
         public string? Image { get; set; }
 
         public int? RoleId { get; set; }
