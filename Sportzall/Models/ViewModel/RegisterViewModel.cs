157080841d1b2a062ca194ba278572168439b62a
DolausNot <dsadasdasdas19@gmail.com>
2022-12-05 23:47:23 +0200

    Add Validation

--- Sportzall/Models/ViewModel/RegisterViewModel.cs
+++ Sportzall/Models/ViewModel/RegisterViewModel.cs
@@ -4,17 +4,21 @@ namespace Sportzall.Models.ViewModel
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Не указан Ім'я")]
+        [Required(ErrorMessage = "Не вказано Ім'я")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Не указан Email")]
+        [MinLength(1), DataType(DataType.EmailAddress), EmailAddress, MaxLength(50), Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Не указан пароль")]
+        [Required]
+        [MinLength(4, ErrorMessage = "Пароль має містити неменше 4 символів")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        
+        [Compare("Password", ErrorMessage = "Пароль введено невірно")]
         public string ConfirmPassword { get; set; }
     }
 }
